4498d293764a41b4c6ed21a8783eb3a643c34b73
maxi <maxitunn17@gmail.com>
2026-03-26 20:06:52 +0100

    add vercel domain to CORS and CSRF

--- Core/settings.py
+++ Core/settings.py
@@ -110,27 +110,24 @@ DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 #in this place put on all the domain that will have access to the applications
 CORS_ALLOWED_ORIGINS = [
-     "http://localhost:3000",
+    "http://localhost:3000",
     "http://localhost:8000",
     "http://127.0.0.1:3000",
-    "http://127.0.0.1:3000",
-  
+    "https://sofigurumi-page.vercel.app",
 ]
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:3000',
- 
-] 
+    'https://sofigurumi-page.vercel.app',
+]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:3000',
-   
+    'https://sofigurumi-page.vercel.app',
 ]
 
 PASSWORD_HASHERS = [ #this is to make all password will be more safe
